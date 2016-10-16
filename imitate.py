3d2e5fd28dd6be559a74ba8e6c1ef14a3fdde722
Jean-Marie Comets <jean.marie.comets@gmail.com>
2016-10-16 18:01:57 +0200

    Allow quick scripted generation of imitation

--- imitate.py
+++ imitate.py
@@ -35,3 +35,42 @@ class MessagesModel(NewlineText):
     def sentence_split(self, text):
         sentences =  super().sentence_split(text)
         return list(it.chain.from_iterable(map(split_into_sentences, sentences)))
+
+if __name__ == '__main__':
+    import sys
+
+    from models import get_registered_profiles
+
+    registered_profiles = get_registered_profiles()
+    registered_nicknames = set(map(lambda p: p.nickname, registered_profiles))
+
+    if len(sys.argv) < 2:
+        print('No nickname given', file=sys.stderr)
+        if registered_nicknames:
+            print('Choose one of:', file=sys.stderr)
+            print('\n'.join(map(lambda n: '* %s' % n, registered_nicknames)), file=sys.stderr)
+        sys.exit(1)
+    nickname = sys.argv[1]
+
+    if nickname not in registered_nicknames:
+        print('Invalid nickname given', file=sys.stderr)
+        if registered_nicknames:
+            print('Choose one of:', file=sys.stderr)
+            print('\n'.join(map(lambda n: '* %s' % n, registered_nicknames)), file=sys.stderr)
+        sys.exit(1)
+
+    n = 2
+    if len(sys.argv) > 2:
+        try:
+            n = int(sys.argv[2])
+            if n <= 0:
+                raise ValueError
+        except ValueError:
+            print('N should be a positive integer', file=sys.stderr)
+
+    profiles_by_nickname = dict(map(lambda p: (p.nickname, p), registered_profiles))
+    messages = list(map(lambda m: m.text, profiles_by_nickname[nickname].messages))
+
+    imitator = Imitator(messages)
+    imitator.index(2)
+    print(imitator.generate_sentence())
